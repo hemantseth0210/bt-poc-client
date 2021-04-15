374a73415c915870cdc047949503e72f29dd60a1
Hemant Seth <hemant.seth@macys.com>
2021-04-15 17:22:18 -0400

    Added 1 sec

--- src/main/java/com/example/bigtablepocclient/BigtableThread.java
+++ src/main/java/com/example/bigtablepocclient/BigtableThread.java
@@ -15,7 +15,8 @@ public class BigtableThread implements Runnable{
     public void run(){
         RestTemplate restTemplate = new RestTemplate();
         //final String baseUrl = "http://localhost:8080/processNcpEligibleUpcsByPrefixPostWithRedisCache";
-        final String baseUrl = "http://104.197.166.156:8080/processNcpEligibleUpcsByPrefixPostWithRedisCache";
+        //final String baseUrl = "http://104.197.166.156:8080/processNcpEligibleUpcsByPrefixPostWithRedisCache";
+        final String baseUrl = "http://104.197.166.156:8080/processNcpEligibleUpcsByPrefixPostWithMemcached";
         URI uri = null;
         try {
             uri = new URI(baseUrl);
@@ -26,9 +27,10 @@ public class BigtableThread implements Runnable{
         HttpHeaders headers = new HttpHeaders();
         int counter = 1;
         while(true){
-           // try {
-                //Thread.sleep(1000);
+            try {
+                Thread.sleep(1000);
                 System.out.println(Thread.currentThread().getName() + " Hitting range service " + counter + " times");
+
                 List<String> finalUpcsList = new ArrayList<>();
                 Collections.shuffle(upcs, new Random(upcs.size()));
                 Random rand = new Random();
@@ -40,9 +42,9 @@ public class BigtableThread implements Runnable{
                 restTemplate.postForEntity(uri, request, Map.class);
                 System.out.println(Thread.currentThread().getName() + " Got the result from range service " + counter + " times");
                 counter++;
-            //} catch (InterruptedException e) {
-            //  e.printStackTrace();
-            //}
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
         }
     }
 
@@ -50,7 +52,7 @@ public class BigtableThread implements Runnable{
         List<String> upcs = new ArrayList<>();
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader("src/main/resources/upcs.txt"));
+            reader = new BufferedReader(new FileReader("/home/hemant_seth/bt-poc-client/src/main/resources/upcs.txt"));
             String line = reader.readLine();
             while (line != null) {
                 upcs.add(line);
